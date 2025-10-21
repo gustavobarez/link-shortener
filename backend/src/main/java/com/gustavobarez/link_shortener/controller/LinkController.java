374d900b22799400573398d2a2bc902df5f5f3ff
gustavobarez <gbarez44@gmail.com>
2025-10-21 10:09:31 -0300

    config: configure cors on controller

--- backend/src/main/java/com/gustavobarez/link_shortener/controller/LinkController.java
+++ backend/src/main/java/com/gustavobarez/link_shortener/controller/LinkController.java
@@ -2,6 +2,7 @@ package com.gustavobarez.link_shortener.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.gustavobarez.link_shortener.service.LinkService;
 
 @RestController
 @RequestMapping("/api/v1/url")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LinkController {
 
     private final LinkService service;
