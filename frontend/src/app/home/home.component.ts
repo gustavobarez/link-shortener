dc2740a463ae95989c7e3965a8ec3dd6ed243f90
gustavobarez <gbarez44@gmail.com>
2025-10-22 13:33:32 -0300

    feat: updates home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DeleteUrlService } from '../services/delete-url.service';
 import { UrlShortenerService } from '../services/url-shortener.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -16,15 +17,26 @@ export class HomeComponent {
   resultPublicId: string = '';
   resultPassword: string = '';
 
+  showDeleteArea: boolean = false;
   deletePublicId: string = '';
   deletePassword: string = '';
 
   shortUrl: string = '';
 
+  showOutput: boolean = false;
+
   constructor(
     private urlService: UrlShortenerService,
     private deleteUrlService: DeleteUrlService
   ) {}
+
+  openLink() {
+    window.open(`http://localhost:8080/api/v1/url/${this.shortUrl}`, '_blank')
+  }
+
+  copy(text: string) {
+    navigator.clipboard.writeText(text);
+  }
 
   shorten() {
     this.urlService.shortenUrl(this.urlInput).subscribe({
@@ -34,10 +46,15 @@ export class HomeComponent {
         this.shortUrl = response.shortUrl;
         this.deletePublicId = this.resultPublicId;
         this.deletePassword = this.resultPassword;
+        this.showOutput = true;
         console.log(response);
       },
       error: (error) => console.log(error),
     });
+  }
+
+  showDelete() {
+    this.showDeleteArea = !this.showDeleteArea;
   }
 
   delete() {
